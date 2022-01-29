1388d39a9d1bfa2f0ca9e4335832f398fb0cb26c
Q-Digital <no-reply@q-digital.org>
2022-01-30 00:24:05 +0400

    Feat: add new realization

--- main.js
+++ main.js
@@ -1,37 +1,51 @@
+const score = document.getElementById('score')
+const restart = document.getElementById('restart')
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 const w = canvas.width
 const h = canvas.height
 
+
+var img = new Image()
+img.src = 'img/forest.jpg'
 var mouse = { x: 0, y: 0 };
 var draw = false;
-const cubeSize = 120
-const cubeCount = 6
+const cubeSize = 150
 const animals = [
     {
         name: 'Олень',
         color: 'red',
-        id: 0,
+        isReady: false,
         path: []
     },
     {
         name: 'Волк',
-        color: 'green',
-        id: 1,
+        color: 'black',
+        isReady: false,
         path: []
     },
     {
         name: 'Птеродактель',
         color: 'blue',
-        id: 2,
+        isReady: false,
         path: []
     },
 ]
-const posArr = []
+const cubeCount = 2 * animals.length
+let posArr = []
 let currentElem = {}
-let counter = 0
+
+const restartGame = () => {
+    animals.map(e => e.isReady = false)
+    posArr = []
+    currentElem = {}
+    score.innerText = 0
+    context.lineWidth = 2;
+    startGame()
+}
 
 const startGame = () => {
+
     for (i = 0; posArr.length < cubeCount; i++) {
         let xPos = Math.round(Math.random() * canvas.width)
         let yPos = Math.round(Math.random() * canvas.height)
@@ -41,7 +55,8 @@ const startGame = () => {
         posArr.find(e => (Math.abs(e.x - xPos) < cubeSize) && (Math.abs(e.y - yPos) < cubeSize)) ? null :
             posArr.push({
                 x: xPos,
-                y: yPos
+                y: yPos,
+                path: []
             })
     }
 
@@ -51,50 +66,78 @@ const startGame = () => {
         e.animal = animals[i].name
     })
 
+    context.drawImage(img, 0, 0, w, h);
+    // context.clearRect(0,0,w,h)
+
     posArr.map(e => {
+        context.lineWidth = 3;
         context.strokeStyle = e.color
         context.strokeRect(e.x, e.y, cubeSize, cubeSize)
-        context.fillText(e.animal, e.x + 10, e.y + cubeSize / 2)
+        context.fillStyle = "#fff";
+        context.font = "22px Muller";
+        context.fillText(e.animal, e.x, e.y + cubeSize / 2)
     })
-
-    canvas.addEventListener("mousedown", function (e) {
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
-        currentElem = posArr.find(elem => (
-            ((mouse.x - elem.x) < cubeSize) && ((mouse.x - elem.x) > 0)
-            && ((mouse.y - elem.y) < cubeSize) && (mouse.y - elem.y) > 0))
-            draw = true;
-            context.beginPath()
-            context.strokeStyle = currentElem.color
-            context.moveTo(mouse.x, mouse.y)
-    });
-
-    canvas.addEventListener("mousemove", function (e) {
-        if (draw == true) {
-            mouse.x = e.pageX - this.offsetLeft;
-            mouse.y = e.pageY - this.offsetTop;
-            context.lineTo(mouse.x, mouse.y);
-            context.lineWidth = 5;
-            context.stroke();
-        }
-    });
-
-    canvas.addEventListener("mouseup", function (e) {
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
-        let nextElem = posArr.find(elem => (
-            ((mouse.x - elem.x) < cubeSize) && ((mouse.x - elem.x) > 0)
-            && ((mouse.y - elem.y) < cubeSize) && (mouse.y - elem.y) > 0))
-
-        context.lineTo(mouse.x, mouse.y);
-        context.stroke();
-        context.closePath();
-        draw = false;
-        if (currentElem.animal === nextElem.animal && currentElem.x !== nextElem.x) {
-            counter++
-            counter === animals.length && alert('you win')
-        }
-    });
 }
 
-startGame()
+canvas.addEventListener("mousedown", function (e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    currentElem = posArr.find(elem => (
+        ((mouse.x - elem.x) < cubeSize) && ((mouse.x - elem.x) > 0)
+        && ((mouse.y - elem.y) < cubeSize) && (mouse.y - elem.y) > 0))
+    draw = true;
+    context.beginPath()
+    context.strokeStyle = currentElem && currentElem.color
+    context.moveTo(mouse.x, mouse.y)
+});
+
+canvas.addEventListener("mousemove", function (e) {
+    if (draw == true) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+        context.lineTo(mouse.x, mouse.y);
+        context.lineWidth = 5;
+        context.stroke();
+
+        // const animal = animals.find(elem => elem.name === currentElem.animal)
+        // animal.path.push([mouse.x, mouse.y])
+
+        const nextElem = posArr.find(elem => (
+            ((mouse.x - elem.x) < cubeSize) && ((mouse.x - elem.x) > 0)
+            && ((mouse.y - elem.y) < cubeSize) && (mouse.y - elem.y) > 0))
+        if (nextElem && currentElem && nextElem.animal !== currentElem.animal) {
+            context.closePath();
+            draw = false;
+            alert('ИгРа ОконЧЕНа =)))')
+        }
+    }
+});
+
+canvas.addEventListener("mouseup", function (e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    let nextElem = posArr.find(elem => (
+        ((mouse.x - elem.x) < cubeSize) && ((mouse.x - elem.x) > 0)
+        && ((mouse.y - elem.y) < cubeSize) && (mouse.y - elem.y) > 0))
+
+    context.lineTo(mouse.x, mouse.y);
+    context.stroke();
+    context.closePath();
+    draw = false;
+    if (currentElem && nextElem && currentElem.animal === nextElem.animal && currentElem.x !== nextElem.x) {
+        const animal = animals.find(e => e.name === currentElem.animal)
+        if (animal) {
+            animal.isReady = true
+            animals.every(e => e.isReady) && alert('you win')
+            score.innerText = animals.filter(e => e.isReady).length + '/' + animals.length
+        }
+    } else alert('Хули творишь')
+});
+
+restart.addEventListener('click', restartGame)
+
+
+
+img.addEventListener("load", function() {
+    startGame()
+});
