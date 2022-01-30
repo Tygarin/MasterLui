f64c00053d5b5006f90207687b9b1968bf07ddbc
Q-Digital <no-reply@q-digital.org>
2022-01-31 01:11:14 +0400

    Ready game

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 const w = canvas.width
 const h = canvas.height
+const modal = document.getElementById('modal')
+const modalText = document.getElementById('modalText')
+const closeModal = document.getElementById('closeModal')
 
 
 var img = new Image()
@@ -16,19 +19,22 @@ const animals = [
         name: 'Олень',
         color: 'red',
         isReady: false,
-        path: []
+        path: [],
+        power: 0,
     },
     {
         name: 'Волк',
         color: 'black',
         isReady: false,
-        path: []
+        path: [],
+        power: 1,
     },
     {
         name: 'Птеродактель',
         color: 'blue',
         isReady: false,
-        path: []
+        path: [],
+        power: 2,
     },
 ]
 const cubeCount = 2 * animals.length
@@ -36,7 +42,10 @@ let posArr = []
 let currentElem = {}
 
 const restartGame = () => {
-    animals.map(e => e.isReady = false)
+    animals.map(e => {
+        e.isReady = false
+        e.path = []
+    })
     posArr = []
     currentElem = {}
     score.innerText = 0
@@ -44,8 +53,12 @@ const restartGame = () => {
     startGame()
 }
 
-const startGame = () => {
+const openModal = (text) => {
+    modal.style.display = 'flex'
+    modalText.innerText = text
+}
 
+const startGame = () => {
     for (i = 0; posArr.length < cubeCount; i++) {
         let xPos = Math.round(Math.random() * canvas.width)
         let yPos = Math.round(Math.random() * canvas.height)
@@ -64,10 +77,10 @@ const startGame = () => {
         if (i >= animals.length) i = i % animals.length
         e.color = animals[i].color
         e.animal = animals[i].name
+        e.power = animals[i].power
     })
 
     context.drawImage(img, 0, 0, w, h);
-    // context.clearRect(0,0,w,h)
 
     posArr.map(e => {
         context.lineWidth = 3;
@@ -79,7 +92,7 @@ const startGame = () => {
     })
 }
 
-canvas.addEventListener("mousedown", function (e) {
+function mouseDown(e) {
     mouse.x = e.pageX - this.offsetLeft;
     mouse.y = e.pageY - this.offsetTop;
     currentElem = posArr.find(elem => (
@@ -89,9 +102,9 @@ canvas.addEventListener("mousedown", function (e) {
     context.beginPath()
     context.strokeStyle = currentElem && currentElem.color
     context.moveTo(mouse.x, mouse.y)
-});
+}
 
-canvas.addEventListener("mousemove", function (e) {
+function mouseMove (e) {
     if (draw == true) {
         mouse.x = e.pageX - this.offsetLeft;
         mouse.y = e.pageY - this.offsetTop;
@@ -99,8 +112,13 @@ canvas.addEventListener("mousemove", function (e) {
         context.lineWidth = 5;
         context.stroke();
 
-        // const animal = animals.find(elem => elem.name === currentElem.animal)
-        // animal.path.push([mouse.x, mouse.y])
+        const animal = animals.find(elem => elem.name === currentElem.animal)
+        animal.path.push([mouse.x, mouse.y])
+
+        if (w - mouse.x <= 5 || h - mouse.y <= 5 || mouse.x <= 5 || mouse.y <= 5) {
+            draw = false;
+            openModal('Вы вышли за рамки')
+        }
 
         const nextElem = posArr.find(elem => (
             ((mouse.x - elem.x) < cubeSize) && ((mouse.x - elem.x) > 0)
@@ -108,12 +126,18 @@ canvas.addEventListener("mousemove", function (e) {
         if (nextElem && currentElem && nextElem.animal !== currentElem.animal) {
             context.closePath();
             draw = false;
-            alert('ИгРа ОконЧЕНа =)))')
+            openModal(currentElem.animal + (currentElem.power > nextElem.power ? ' съел животное ' : ' был съеден животным ') + nextElem.animal)
+        }
+
+        let cross = animals.find(e => e.name !== currentElem.animal && e.path.find(elem => ((Math.abs(elem[0] - mouse.x) < 13) && (Math.abs(elem[1] - mouse.y) < 13))))
+        if (cross) {
+            openModal(currentElem.animal + (currentElem.power > cross.power ? ' съел животное ' : ' был съеден животным ') + cross.name)
+            draw = false
         }
     }
-});
+}
 
-canvas.addEventListener("mouseup", function (e) {
+function mouseUp (e) {
     mouse.x = e.pageX - this.offsetLeft;
     mouse.y = e.pageY - this.offsetTop;
     let nextElem = posArr.find(elem => (
@@ -128,16 +152,25 @@ canvas.addEventListener("mouseup", function (e) {
         const animal = animals.find(e => e.name === currentElem.animal)
         if (animal) {
             animal.isReady = true
-            animals.every(e => e.isReady) && alert('you win')
+            animals.every(e => e.isReady) && openModal('Победа!!! :))')
             score.innerText = animals.filter(e => e.isReady).length + '/' + animals.length
         }
-    } else alert('Хули творишь')
-});
+    } else openModal('не делайте так')
+}
+
+canvas.addEventListener("mousemove", mouseMove);
+
+canvas.addEventListener("mousedown", mouseDown);
+
+canvas.addEventListener("mouseup", mouseUp);
+
+closeModal.addEventListener('click', e => {
+    modal.style.display = 'none'
+    restartGame()
+})
 
 restart.addEventListener('click', restartGame)
 
-
-
-img.addEventListener("load", function() {
+img.addEventListener("load", function () {
     startGame()
 });
